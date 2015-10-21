6c836da34abe7a9056e32cc57664b345aff05a2c
Espen A. Kristiansen <post@espenak.net>
2015-10-21 14:58:25 +0200

    Set version to 0.0.2.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name="transloadit",
-      version="0.0.1",
+      version="0.0.2",
       description="Library for interfacing with Transloadit's API",
       author="Joe Stump",
       author_email="joe@joestump.net",
